542a6e9b41dc799f9e92e69252f69c30a93a2837
rua-iri <rory.mcguigan@protonmail.com>
2023-02-27 13:47:03 +0000

    add feature to embed messages within xml tags making them easier to find

--- imageProcessor.py
+++ imageProcessor.py
@@ -8,6 +8,18 @@ def checkIsLongEnough(msg, wdth, hght):
     else:
         return False
 
+
+
+# function to return a list of the binary representations of the xml tags in which messages are enclosed
+def getXmlTags():
+    xTags = []
+    xTags.append(''.join(format(ord(c), '08b') for c in "<msg>"))
+    xTags.append(''.join(format(ord(c), '08b') for c in "</msg>"))
+    return xTags
+
+
+
+# function to encode message in a given image
 def writeToImage(imgName, textToEncode):
 
     img = Image.open(imgName)
@@ -19,12 +31,18 @@ def writeToImage(imgName, textToEncode):
     for ltr in textToEncode:
         binaryMessage += format(ord(ltr), "08b")
 
-    
+
+    # enclose the message in the binary respresentation of the xml tags that 
+    # signify the start and end of the message
+    openTag, closeTag = getXmlTags()
+    binaryMessage = openTag + binaryMessage + closeTag
 
     # TODO use checkIsLongEnough function before the rest of stuff is executed
 
 
     # TODO check if file is in a lossless compression format
+    # probably using the get_format_mimetype() method on the image
+    # alternatively we could accept all filetypes but EXPORT as png only
 
 
     # TODO add xml markers to signify where the message begins and ends
@@ -33,17 +51,16 @@ def writeToImage(imgName, textToEncode):
     # TODO encode the message at a random location within the file
 
 
-    # TODO remove this
-    print(imgPixels[:33])
-    for i in range(len(imgPixels[:33])):
-        with open("imgInFile.txt", "a") as imgFile:
-            imgFile.write(str(imgPixels[i]) + "\n")
+    # TODO forbid users from including <msg> or </msg> in their messages
+
+    # TODO maybe add a second type of encryption for the message contents
 
 
+    # determine the max number of pixels that must be modified to encode the message
     requiredPixels = len(binaryMessage) // 3 + 1
 
     bIndex = 0
-    # iterate through each bit in the binary message
+    # iterate through each pixel to be modified
     for i in range(requiredPixels):
 
             # convert to list so that values can be reassigned
@@ -59,13 +76,6 @@ def writeToImage(imgName, textToEncode):
             
             imgPixels[i] = tuple(imgPixels[i])
 
-
-
-    # TODO remove this
-    print(imgPixels[:33])
-    for i in range(len(imgPixels[:33])):
-        with open("imgOutFile.txt", "a") as imgFile:
-            imgFile.write(str(imgPixels[i]) + "\n")
 
     # write to a new image
     encodedImg = Image.new(img.mode, img.size)
@@ -87,38 +97,37 @@ def readFromImage(fileName):
     for px in imgPixels:
         binaryData += bin(px[0])[-1] + bin(px[1])[-1] + bin(px[2])[-1]
 
-    # TODO remove this
-    for i in range(len(imgPixels[:33])):
-        with open("imgDecodeFile.txt", "a") as imgFile:
-            imgFile.write(str(imgPixels[i]) + "\n")
 
     # TODO remove this
     print(binaryData.find("010010000110010101101100011011000110111100101100001000000101011101101111011100100110110001100100"))
     
 
+    # find the start and the end using the binary values for the xml tags
+    openTag, closeTag = getXmlTags()
+    msgStart = binaryData.find(openTag) + len(openTag)
+    msgEnd = binaryData.find(closeTag)
 
-    # # decode binary into readable text
-    # msgText= ""
+    # decode binary into readable text
+    totalText= ""
     # for i in range(len(binaryData)//8):
-    #     charStart = i*8
-    #     charEnd = (i+1)*8
-    #     msgText += chr(int(binaryData[charStart:charEnd], 2))
+    for i in range((msgStart//8), (msgEnd//8)):
+        charStart = i*8
+        charEnd = (i+1)*8
+        totalText += chr(int(binaryData[charStart:charEnd], 2))
 
-    # # TODO remove this
-    # with open("Blah.txt", "w") as blahFile:
-    #     blahFile.write(msgText[:30])
+    # TODO remove this
+    with open("Blah.txt", "w") as blahFile:
+        blahFile.write(totalText)
 
-    # print(msgText[:20])
-    # print(len(msgText))
-
-
-
+    print(totalText)
+    print(len(totalText))
 
 
 
-writeToImage("testImage.png", "Hello, World")
+
+
+# writeToImage("testImage.png", "This message is a test...")
 readFromImage("encoded_testImage.png")
-
 
 
 
