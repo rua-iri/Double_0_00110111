0b1764a02d59a4f22f73a3aaceb4a81dd5206ebd
rua-iri <rory.mcguigan@protonmail.com>
2023-02-28 00:02:45 +0000

    add function to embed the given message at a random location in the file

--- imageProcessor.py
+++ imageProcessor.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import random
 
 
 # function to check if image is large enough to store message
@@ -16,6 +17,17 @@ def getXmlTags():
     xTags.append(''.join(format(ord(c), '08b') for c in "<msg>"))
     xTags.append(''.join(format(ord(c), '08b') for c in "</msg>"))
     return xTags
+
+
+# function to get a random location in the image to encode the message
+def getEncodeLocation(reqPixels, maxPixels):
+    encodeLocation = random.randint(0, (maxPixels-reqPixels))
+
+    # regenerate encodeLocation until it is divisible by 8
+    while encodeLocation%8:
+        encodeLocation = random.randint(0, (maxPixels-reqPixels))
+
+    return encodeLocation
 
 
 
@@ -39,18 +51,6 @@ def writeToImage(imgName, textToEncode):
 
     # TODO use checkIsLongEnough function before the rest of stuff is executed
 
-
-    # TODO check if file is in a lossless compression format
-    # probably using the get_format_mimetype() method on the image
-    # alternatively we could accept all filetypes but EXPORT as png only
-
-
-    # TODO add xml markers to signify where the message begins and ends
-
-
-    # TODO encode the message at a random location within the file
-
-
     # TODO forbid users from including <msg> or </msg> in their messages
 
     # TODO maybe add a second type of encryption for the message contents
@@ -59,9 +59,18 @@ def writeToImage(imgName, textToEncode):
     # determine the max number of pixels that must be modified to encode the message
     requiredPixels = len(binaryMessage) // 3 + 1
 
+    # TODO Remove this
+    print("Required Pixels: " + str(requiredPixels))
+    print("Total Pixels: " + str(len(imgPixels)))
+    print("Start Pixels: " + str(len(imgPixels) - requiredPixels))
+
+    # generate random encode location
+    encodeLocation = getEncodeLocation(requiredPixels, len(imgPixels))
+    
+
     bIndex = 0
     # iterate through each pixel to be modified
-    for i in range(requiredPixels):
+    for i in range(encodeLocation, (encodeLocation+requiredPixels)):
 
             # convert to list so that values can be reassigned
             imgPixels[i] = list(imgPixels[i])
@@ -77,39 +86,39 @@ def writeToImage(imgName, textToEncode):
             imgPixels[i] = tuple(imgPixels[i])
 
 
-    # write to a new image
+    # generate a new name for the file to be saved
+    newFileName = str(imgName.split(".")[0]) + "_encoded" +  ".png"
+
+    # write to a new image (must be a png file for lossless compression)
     encodedImg = Image.new(img.mode, img.size)
     encodedImg.putdata(imgPixels)
-    encodedImg.save("encoded_" + imgName)
+    encodedImg.save(fp=newFileName, format="PNG")
 
 
 
 
-
+# function to extract secret message from a file
 def readFromImage(fileName):
     img = Image.open(fileName)
     imgPixels = list(img.getdata())
 
     # concatenate the data from the least significant bit of every pixel
     binaryData = ""
-
-
     for px in imgPixels:
         binaryData += bin(px[0])[-1] + bin(px[1])[-1] + bin(px[2])[-1]
-
-
-    # TODO remove this
-    print(binaryData.find("010010000110010101101100011011000110111100101100001000000101011101101111011100100110110001100100"))
     
 
     # find the start and the end using the binary values for the xml tags
     openTag, closeTag = getXmlTags()
     msgStart = binaryData.find(openTag) + len(openTag)
     msgEnd = binaryData.find(closeTag)
+    
+    # TODO Remove this
+    print("Message Start: " + str(binaryData.find(openTag)))
+    print("Message End: " + str(msgEnd))
 
     # decode binary into readable text
     totalText= ""
-    # for i in range(len(binaryData)//8):
     for i in range((msgStart//8), (msgEnd//8)):
         charStart = i*8
         charEnd = (i+1)*8
@@ -119,15 +128,16 @@ def readFromImage(fileName):
     with open("Blah.txt", "w") as blahFile:
         blahFile.write(totalText)
 
-    print(totalText)
-    print(len(totalText))
+    # TODO Remove this
+    print("Total Text: '" + totalText + "'")
+
+
+    # TODO return message text or pass to another program
 
 
 
-
-
-# writeToImage("testImage.png", "This message is a test...")
-readFromImage("encoded_testImage.png")
+writeToImage("testImage.png", "I am putting a secret message at a random location")
+readFromImage("testImage_encoded.png")
 
 
 
