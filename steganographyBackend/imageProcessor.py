2ae7e04bd43bf586dd55509655afb65a7f5daea0
rua-iri <rory.mcguigan@protonmail.com>
2023-03-08 14:06:25 +0000

    alter directory structure and add default lambda code

--- steganographyBackend/imageProcessor.py
+++ steganographyBackend/imageProcessor.py
@@ -1,5 +1,29 @@
 from PIL import Image
 import random
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+
 
 
 # function to check if image is large enough to store message
@@ -133,6 +157,9 @@ def readFromImage(fileName):
 
 
     # TODO return message text or pass to another program
+
+    # TODO return an error message if the xml tags are not found
+    # indicating that no message was encoded in the image
 
 
 
