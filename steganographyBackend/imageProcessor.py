7c6d97ee5102bdd94d352b5e19d63766e1339eb9
rua-iri <rory.mcguigan@protonmail.com>
2023-03-09 16:39:55 +0000

    move files to one directory and add support for command line arguments

--- steganographyBackend/imageProcessor.py
+++ steganographyBackend/imageProcessor.py
@@ -1,5 +1,32 @@
+import sys
 from PIL import Image
 import random
+
+def main(argv):
+
+    # check that the right number of arguments has been passed
+    # and call different function depending on the first argument
+    if len(argv)==3 and argv[0]=="encode":
+
+        imgPath = argv[1]
+        encodeString = argv[2]
+        print(imgPath)
+        print(encodeString)
+
+        writeToImage(imgPath, encodeString)
+
+    elif len(argv)==2 and argv[0]=="decode":
+        imgPath = argv[1]
+        print(imgPath)
+
+        readFromImage(imgPath)
+
+
+    # TODO return error message if arguments are wrong
+    else:
+        return False
+
+        
 
 
 # function to check if image is large enough to store message
@@ -8,7 +35,6 @@ def checkIsLongEnough(msg, wdth, hght):
         return True
     else:
         return False
-
 
 
 # function to return a list of the binary representations of the xml tags in which messages are enclosed
@@ -34,7 +60,6 @@ def getEncodeLocation(reqPixels, maxPixels):
 # function to encode message in a given image
 def writeToImage(imgName, textToEncode):
 
-
     img = Image.open(imgName)
     imgWidth, imgHeight = img.size
     imgPixels = list(img.getdata())
@@ -48,54 +73,61 @@ def writeToImage(imgName, textToEncode):
     # enclose the message in the binary respresentation of the xml tags that 
     # signify the start and end of the message
     openTag, closeTag = getXmlTags()
+
+    # check that binary message does not include xml tags before they are added
+    isValidMessage = False if openTag in binaryMessage or closeTag in binaryMessage else True
+
     binaryMessage = openTag + binaryMessage + closeTag
 
-    # TODO use checkIsLongEnough function before the rest of stuff is executed
+    isMessageLongEnough = checkIsLongEnough(binaryMessage, imgWidth, imgHeight)
 
-    # TODO forbid users from including <msg> or </msg> in their messages
 
     # TODO maybe add a second type of encryption for the message contents
 
+    # use checkIsLongEnough to ensure that image has enough pixels
+    if isValidMessage and isMessageLongEnough:
 
-    # determine the max number of pixels that must be modified to encode the message
-    requiredPixels = len(binaryMessage) // 3 + 1
+        # determine the max number of pixels that must be modified to encode the message
+        requiredPixels = len(binaryMessage) // 3 + 1
 
-    # TODO Remove this
-    print("Required Pixels: " + str(requiredPixels))
-    print("Total Pixels: " + str(len(imgPixels)))
-    print("Start Pixels: " + str(len(imgPixels) - requiredPixels))
+        # TODO Remove this
+        print("Required Pixels: " + str(requiredPixels))
+        print("Total Pixels: " + str(len(imgPixels)))
+        print("Start Pixels: " + str(len(imgPixels) - requiredPixels))
 
-    # generate random encode location
-    encodeLocation = getEncodeLocation(requiredPixels, len(imgPixels))
-    
+        # generate random encode location
+        encodeLocation = getEncodeLocation(requiredPixels, len(imgPixels))
+        
 
-    bIndex = 0
-    # iterate through each pixel to be modified
-    for i in range(encodeLocation, (encodeLocation+requiredPixels)):
+        bIndex = 0
+        # iterate through each pixel to be modified
+        for i in range(encodeLocation, (encodeLocation+requiredPixels)):
 
-            # convert to list so that values can be reassigned
-            imgPixels[i] = list(imgPixels[i])
+                # convert to list so that values can be reassigned
+                imgPixels[i] = list(imgPixels[i])
 
-            # iterate through each colour value for each pixel
-            for x in range(len(imgPixels[i])):
+                # iterate through each colour value for each pixel
+                for x in range(len(imgPixels[i])):
 
-                # check if i < binaryMessage length
-                if bIndex < len(binaryMessage):
-                    imgPixels[i][x] = int(bin(imgPixels[i][x])[:-1] + binaryMessage[bIndex], 2)
-                    bIndex+=1
-            
-            imgPixels[i] = tuple(imgPixels[i])
+                    # check if i < binaryMessage length
+                    if bIndex < len(binaryMessage):
+                        imgPixels[i][x] = int(bin(imgPixels[i][x])[:-1] + binaryMessage[bIndex], 2)
+                        bIndex+=1
+                
+                imgPixels[i] = tuple(imgPixels[i])
 
 
-    # generate a new name for the file to be saved
-    newFileName = str(imgName.split(".")[0]) + "_encoded" +  ".png"
+        # generate a new name for the file to be saved
+        stemFileName = imgName.split("/")[-1]
+        stemFileName = str(stemFileName.split(".")[0])
+        newFileName = "./downloads/" + stemFileName + "_encoded" +  ".png"
 
-    # write to a new image (must be a png file for lossless compression)
-    encodedImg = Image.new(img.mode, img.size)
-    encodedImg.putdata(imgPixels)
-    encodedImg.save(fp=newFileName, format="PNG")
+        # write to a new image (must be a png file for lossless compression)
+        encodedImg = Image.new(img.mode, img.size)
+        encodedImg.putdata(imgPixels)
+        encodedImg.save(fp=newFileName, format="PNG")
 
-    # TODO delete original image after new image has been uploaded
+        # TODO delete original image after new image has been uploaded
 
 
 
@@ -143,4 +175,6 @@ def readFromImage(fileName):
 
 
 
+if __name__=="__main__":
+    main(sys.argv[1:])
 
