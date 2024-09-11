96a8cfab5b730c9d8cfeef00c9d651ae2b41a8a2
rua-iri <rory.mcguigan@protonmail.com>
2024-09-11 10:22:30 +0300

    add: pytest file

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_hello_world():
+    response = client.get("/helloworld")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
