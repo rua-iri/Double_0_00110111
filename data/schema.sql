826915b015311eb3fe1b7f79001b5dc892c9dcfb
rua-iri <rory.mcguigan@protonmail.com>
2025-05-18 00:18:34 +0300

    add: postgres database container

--- data/schema.sql
+++ data/schema.sql
@@ -2,7 +2,7 @@ create table "images" (
   "id" serial primary key,
   "image_filename" varchar(255) not null,
   "image_uuid" varchar(255) not null,
-  "timestamp" DATETIME not null,
+  "timestamp" varchar(255) not null DEFAULT NOW(),
   "image_location" varchar(255) not null,
   "is_processed" BOOLEAN not null
 )
