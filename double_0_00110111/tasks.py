f9d950278f3ed155e31e05a0d1a5ec140db54605
rua-iri <rory.mcguigan@protonmail.com>
2025-05-19 18:18:35 +0300

    add: tasks file for Celery

---
+++ double_0_00110111/tasks.py
@@ -0,0 +1,39 @@
+
+
+from celery import Celery
+import time
+
+from double_0_00110111.db_dao import DB_DAO
+from double_0_00110111.helpers import get_img_local, write_to_image
+
+app = Celery(
+    "tasks",
+    broker="pyamqp://rabbit:mq@localhost:5673//"
+)
+
+
+@app.task
+def add(x, y):
+    time.sleep(3)
+    print(x+y)
+    return x+y
+
+
+@app.task
+def process_image_encoding(image_uuid: str):
+    db_dao = DB_DAO()
+    result = db_dao.select_record_by_uuid(image_uuid)
+
+    full_image_path: str = f""
+    image_data = get_img_local(full_image_path)
+
+    write_to_image(
+        image_data=,
+        messageText=)
+
+    db_dao.update_image_processed_status(image_uuid)
+    return result
+
+
+@app.task
+def decode_image(image_uuid: str): pass
