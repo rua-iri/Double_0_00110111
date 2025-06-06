e6385db33c532b9d2b6a544b8fa29faf721f631e
rua-iri <rory.mcguigan@protonmail.com>
2025-06-06 04:11:38 +0300

    update: implement responses from get image api

--- nextjs_frontend/src/app/api/getImage/route.tsx
+++ nextjs_frontend/src/app/api/getImage/route.tsx
@@ -1,27 +1,64 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  let endpointURL: string = `${process.env.BASE_BACKEND_URL}/image/`;
-
   try {
     console.log(request);
     console.log(request.nextUrl.searchParams.get("id"));
     const imgId = request.nextUrl.searchParams.get("id");
-    endpointURL += imgId;
 
-    const response = await fetch(endpointURL);
-    console.log(response);
-    console.log(response.body);
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-    const imgBase64 = buffer.toString("base64");
+    if (!imgId) {
+      throw new Error("No image ID provided");
+    }
+
+    const imgBase64 = await getImageContent(imgId);
+
+    console.log(imgBase64);
 
     console.log("Decoding image");
-    return Response.json({
-      status: "success",
-      imgData: imgBase64,
+    return NextResponse.json({
+      status: 200,
+      body: {
+        imgData: imgBase64,
+      },
     });
   } catch (error) {
-    console.log(error);
+    if (error instanceof NotFoundError) {
+      return NextResponse.json({
+        status: 404,
+        error: "Image not found",
+      });
+    }
+
+    return NextResponse.json({
+      status: 500,
+      error: "Something went wrong",
+    });
+  }
+}
+
+async function getImageContent(imgID: string) {
+  const endpointURL: string = `${process.env.BASE_BACKEND_URL}/image/${imgID}`;
+
+  const response = await fetch(endpointURL);
+  console.log(response);
+  console.log(response.body);
+
+  if (response.status == 202) {
+    // TODO: retry request here
+  }
+
+  if (response.status == 404) {
+    throw new NotFoundError();
+  }
+
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  return buffer.toString("base64");
+}
+
+class NotFoundError extends Error {
+  constructor(message: string = "File not found") {
+    super(message);
+    this.name = "NotFoundError";
   }
 }
--- nextjs_frontend/src/app/app/image/page.tsx
+++ nextjs_frontend/src/app/app/image/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
       setHasImageLoaded(true);
     } catch (error) {
       // TODO: handle error by displaying a message
+      console.error(error);
     }
   }
 
