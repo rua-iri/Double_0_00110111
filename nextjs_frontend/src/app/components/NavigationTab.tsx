cb5d7cf46c7f98a69a2b8ef1a1485f274eb4c44f
rua-iri <rory.mcguigan@protonmail.com>
2025-07-18 17:57:33 +0300

    fix: links not spanning the entire element

--- nextjs_frontend/src/app/components/NavigationTab.tsx
+++ nextjs_frontend/src/app/components/NavigationTab.tsx
@@ -10,7 +10,7 @@ export default function NavigationTab({
   return (
     <div className="manilla-file [writing-mode:vertical-lr] h-32 w-8 text-center text-wrap rounded-r-3xl relative">
       <div className="absolute inset-0 bg-amber-900/20 rounded-r-3xl z-10"></div>
-      <a className="relative z-20" href={tabLink}>
+      <a className="relative z-20 block w-full h-full" href={tabLink}>
         {tabText}
       </a>
     </div>
